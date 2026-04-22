9f43fd09119a3b70e934b5dda846105705d66544
wafaaidan <wafa.aidan@gmail.com>
2026-04-22 07:36:44 +0700

    deklarasi tampilInfo()

--- base/Produk.java
+++ base/Produk.java
@@ -1,5 +1,23 @@
 package base;
 
 public class Produk {
-    
+    private String nama;
+    String Kategori;
+    protected double harga;
+    public int stok;
+
+    public Produk(String nama, double harga, int stok, String Kategori) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+        this.Kategori = Kategori;
+    }
+
+    public void tampilInfo() {
+        System.out.println("Produk  : " + nama);
+        System.out.println("Kategori: " + Kategori);
+        System.out.println("Harga   : " + harga);
+        System.out.println("Stok    : " + stok);
+        System.out.println("---------------------");
+    }
 }
