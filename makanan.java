0d01cc3fbac619ea13bbf4560fa715e7e5aff160
wafaaidan <wafa.aidan@gmail.com>
2026-05-05 10:08:17 +0700

    create file makanan

---
+++ makanan.java
@@ -0,0 +1,20 @@
+
+
+public class makanan extends Produk {
+    private int expired;
+    public makanan(String nama, double harga, int stok, int expired) {
+        super(nama, harga, stok, nama);
+        this.expired = expired;
+    }
+    public void tampilData(){
+        super.tampilInfo();
+        System.out.println("Masa Expired:" + expired + "hari");
+    }
+    public void setExpired(int expired){
+        if(expired < 0) {
+            System.out.println("Masa Expired tidak boleh kurang dari 1");
+        } else{
+            this.expired = expired;
+        }
+    }
+}
