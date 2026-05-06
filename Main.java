f0ce3ec4f3814ea498b4da97c5ef03c7d970d47e
wafaaidan <wafa.aidan@gmail.com>
2026-05-06 09:52:45 +0700

    ubah baris

--- Main.java
+++ Main.java
@@ -3,10 +3,7 @@ public class Main {
         elektronik mesinCuci = new elektronik("sharp", 249.99, 24, "alat rumah tangga", 5 );
         makanan snack = new makanan("Snigger", 0.98, 4550, "Snack", 12);
 
-        mesinCuci.setGaransi(4);
-        mesinCuci.tampilInfo();
-        snack.setExpired(15);
-        snack.tampilInfo();
+        
 
         
     }
