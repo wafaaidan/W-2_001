0349ec90517d7e9b590641a559bfc59203712429
wafaaidan <wafa.aidan@gmail.com>
2026-05-06 10:01:29 +0700

    create object

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        elektronik mesinCuci = new elektronik("sharp", 249.99, 24, "alat rumah tangga", 5 );
-        makanan snack = new makanan("Snigger", 0.98, 4550, "Snack", 12);
+    elektronik laptop = new elektronik("Laptop Asus", "Asus", 85000000, 10, 2);
+    elektronik hp = new elektronik("HP Samsung", "Samsung", 3200000, 25, 1)
+    makanan indomie = new makanan("Indomie Goreng",3500, 100, "1939-03-29", "Mie");
+    makanan roti = new makanan("Roti Tawar", 8000, 50, "2026-04-30", "Roti");
+    
 
         
 
